6f0ade3accc9e59722e664b96a4f289c391575de
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-12-04 21:18:21 +0000

    notes

--- packages/expo-nativescript-adapter/hooks/postinstall.js
+++ packages/expo-nativescript-adapter/hooks/postinstall.js
@@ -1,1 +1,9 @@
+/**
+ * By calling this, each entry specified in the `nativescript.hooks` field in this
+ * plugin's package.json will be written into the app's own "hooks" folder with a
+ * sanitised name corresponding to the project and into a subfolder that
+ * corresponds to the hook.
+ * @example demo-react/hooks/after-prepare/nativescript-community-expo-nativescript-adapter.js
+ * @see @nativescript/hook/index.js
+ */
 require('@nativescript/hook')(require("path").dirname(__dirname)).postinstall();
--- packages/expo-nativescript-adapter/hooks/preuninstall.js
+++ packages/expo-nativescript-adapter/hooks/preuninstall.js
@@ -1,1 +1,8 @@
-require('@nativescript/hook')(require("path").dirname(__dirname)).preuninstall();
+require('@nativescript/hook')(require("path").dirname(__dirname)).preuninstall();/**
+ * By calling this, each entry specified in the `nativescript.hooks` field in this
+ * plugin's package.json will be removed from the app's own "hooks" folder where it
+ * had originally been written with a sanitised name corresponding to the project
+ * and into a subfolder that corresponded to the hook.
+ * @example demo-react/hooks/after-prepare/nativescript-community-expo-nativescript-adapter.js
+ * @see @nativescript/hook/index.js
+ */
