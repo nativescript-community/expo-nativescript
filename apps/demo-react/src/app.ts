53611ff2368d13645d65bc25b8f71cb6766372a0
Jamie Birch work laptop <jbizzle5@gmail.com>
2020-12-12 22:02:59 +0000

    Successfully compiled react-native-shim to native code, and got the Android app to build with the @nativescript-community/expo-permissions instead of expo-permissions.

--- apps/demo-react/src/app.ts
+++ apps/demo-react/src/app.ts
@@ -12,6 +12,20 @@ purpose of the file is to pass control to the app’s first module.
 //@ts-ignore
 console.log("org.unimodules.core:", org.unimodules.core);
 //@ts-ignore
+console.log("expo.modules.permissions:", expo.modules.permissions);
+//@ts-ignore
+console.log("expo.modules.permissions.PermissionsService:", expo.modules.permissions.PermissionsService);
+//@ts-ignore
+console.log("expo.modules.permissions.PermissionsModule:", expo.modules.permissions.PermissionsModule);
+//@ts-ignore
+console.log("expo.modules.permissions.requesters:", expo.modules.permissions.requesters);
+//@ts-ignore
+console.log("expo.modules.permissions.requesters.LocationRequester:", expo.modules.permissions.requesters.LocationRequester);
+//@ts-ignore
+console.log("com.facebook.react.modules.core:", com.facebook.react.modules.core);
+//@ts-ignore
+console.log("com.facebook.react.modules.core.PermissionAwareActivity:", com.facebook.react.modules.core.PermissionAwareActivity);
+//@ts-ignore
 console.log("org.unimodules.core.BasePackage:", org.unimodules.core.BasePackage);
 
 import * as ReactNativeScript from 'react-nativescript';
@@ -23,3 +37,34 @@ ReactNativeScript.start(React.createElement(AppContainer, {}, null));
 Do not place any code after the application has been started as it will not
 be executed on iOS.
 */
+
+/* BEGIN NOTE TO SELF: How I got expo.modules.permissions (though apparently none of its constituen Kotlin files) to compile for Android.
+ * I used @nativescript-community/expo-permissions for the Android implementation
+ * and @nativescript-community/expo-permissions-nativescript-plugin + expo-permissions to provide both the iOS implementation and
+ * cross-platform TS implementation.
+ * @nativescript-community/react-native-shim may or may not have been needed. Maybe it unexpectedly just worked.
+ * As a next step, we could move the contents of @nativescript-community/expo-permissions into the android folder of
+ * @nativescript-community/expo-permissions-nativescript-plugin.
+ */
+// rootProject.name = "plugindemoreact"
+// include ':app'//, ':runtime', ':runtime-binding-generator'
+
+// //project(':runtime').projectDir = new File("${System.env.ANDROID_RUNTIME_HOME}/test-app/runtime")
+// //project(':runtime-binding-generator').projectDir = new File("${System.env.ANDROID_RUNTIME_HOME}/test-app/runtime-binding-generator")
+
+// file("google-services.json").renameTo(file("./app/google-services.json"))
+
+// include ':expo-permissions'
+// project(':expo-permissions').projectDir = new File(rootProject.projectDir, '../../node_modules/@nativescript-community/expo-permissions/platforms/android')
+
+// // throw new GradleException('Got this far: ' + project(':expo-permissions').projectDir);
+
+// include ':unimodules-permissions-interface'
+// project(':unimodules-permissions-interface').projectDir = new File(rootProject.projectDir, '../../node_modules/unimodules-permissions-interface/android')
+
+// /** ':unimodules-core' injected support START **/
+// // Everything from here until the corresponding END comment will be subject to text replacement upon '@nativescript-community/expo-nativescript-adapter' plugin installation.
+// include ':unimodules-core'
+// project(':unimodules-core').projectDir = new File(rootProject.projectDir, '../../node_modules/@unimodules/core/android')
+// /** ':unimodules-core' injected support END **/
+/* END NOTE TO SELF */
